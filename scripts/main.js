7a1130bcfda89080f0e44313edd7cbda1f4cc479
Bodrul Alam <bodrul2112@gmail.com>
2013-06-04 18:19:03 +0100

    third update

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+
+
+require( ["plugins/domReady","thirdparty/jquery"], function(domReady, jQuery){
+	
+	domReady(function(){
+		
+		require(["thirdparty/jquery"], function(jQuery)
+        {
+		    console.log("yo");
+
+		});
+		
+	});
+
+});
+
---
+++ scripts/util/eventhub.js
@@ -0,0 +1,31 @@
+define( function(){
+
+
+    var EventHub = function()
+    {
+        this.m_mEventToCallBacks = {};
+    }
+
+    EventHub.prototype.registerEvent = function()
+    {
+
+    }
+
+    EventHub.protoype.removeEvent = function()
+    {
+
+    }
+
+    EventHub.prototype.addSubscriptionToEvent = function( sEventName, oCallbackImpl )
+    {
+
+    }
+
+    EventHub.prototype.removeSubscriptionToEvent = function( sEventName, oCallbackImpl )
+    {
+
+    }
+
+    return User;
+
+});
